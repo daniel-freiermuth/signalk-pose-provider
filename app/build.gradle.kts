97acad71df1b584796ad009e428406fa62d3eef8
Daniel <d_freiermu14@cs.uni-kl.de>
2026-09-06 10:10:43 +0200

    deps: upgrade activity-compose from 1.9.3 to 1.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -149,7 +149,7 @@ composeCompiler {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
